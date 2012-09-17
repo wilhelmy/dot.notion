1490d8972c7b690503128b94e85c7291519b5b34
Moritz Wilhelmy <mw@barfooze.de>
2012-09-17 23:30:03 +0200

    add my own theme, right now a trivial modification of look_simpleblue.lua

---
+++ look_mw.lua
@@ -0,0 +1,112 @@
+-- look_simpleblue.lua drawing engine configuration file for Notion.
+
+if not gr.select_engine("de") then return end
+
+de.reset()
+
+local FONT = "-*-fixed-medium-r-*-*-12-*-*-*-*-*-*-*"
+
+de.defstyle("*", {
+    shadow_colour = "black",
+    highlight_colour = "black",
+    background_colour = "#0f1f4f",
+    foreground_colour = "#9f9f9f",
+    padding_pixels = 1,
+    highlight_pixels = 0,
+    shadow_pixels = 0,
+    border_style = "elevated",
+    font = FONT,
+    text_align = "center",
+})
+
+de.defstyle("frame", {
+    shadow_colour = "black",
+    highlight_colour = "black",
+    padding_colour = "black",
+    background_colour = "black",
+    foreground_colour = "#ffffff",
+    padding_pixels = 0,
+    highlight_pixels = 0,
+    shadow_pixels = 0,
+    de.substyle("active", {
+        shadow_colour = "black",
+        highlight_colour = "black",
+        background_colour = "black",
+        foreground_colour = "#ffffff",
+    }),
+})
+
+de.defstyle("tab", {
+    font = FONT,
+    de.substyle("active-selected", {
+        shadow_colour = "black",
+        highlight_colour = "black",
+        background_colour = "#3f3f3f",
+        foreground_colour = "#bfbfbf",
+    }),
+    de.substyle("active-unselected", {
+        shadow_colour = "black",
+        highlight_colour = "black",
+        background_colour = "#0f1f4f",
+        foreground_colour = "#9f9f9f",
+    }),
+    de.substyle("inactive-selected", {
+        shadow_colour = "black",
+        highlight_colour = "black",
+        background_colour = "#1f2f4f",
+        foreground_colour = "#bfbfbf",
+    }),
+    de.substyle("inactive-unselected", {
+        shadow_colour = "black",
+        highlight_colour = "black",
+        background_colour = "#0f1f4f",
+        foreground_colour = "#9f9f9f",
+    }),
+    text_align = "center",
+})
+
+de.defstyle("input", {
+    shadow_colour = "black",
+    highlight_colour = "black",
+    background_colour = "#3f3f3f",
+    foreground_colour = "white",
+    padding_pixels = 1,
+    highlight_pixels = 0,
+    shadow_pixels = 0,
+    border_style = "elevated",
+    de.substyle("*-cursor", {
+        background_colour = "white",
+        foreground_colour = "#3f3f3f",
+    }),
+    de.substyle("*-selection", {
+        background_colour = "black",
+        foreground_colour = "white",
+    }),
+})
+
+de.defstyle("input-menu", {
+    padding_pixels=0,
+})
+
+dopath("lookcommon_clean")
+
+de.defstyle("stdisp", {
+    shadow_pixels = 0,
+    highlight_pixels = 0,
+    text_align = "left",
+    background_colour = "#000000",
+    foreground_colour = "grey",
+    font = FONT,
+    
+    de.substyle("important", {
+        foreground_colour = "green",
+    }),
+
+    de.substyle("critical", {
+        foreground_colour = "red",
+    }),
+})
+
+
+gr.refresh()
+
