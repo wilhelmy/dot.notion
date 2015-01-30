6964206afeb18487f299adc6c7225c3e107e407d
Moritz Wilhelmy <mw@barfooze.de>
2015-01-30 17:22:46 +0100

    Add this stupid linux battery plugin (rewrite later)

--- cfg_statusbar.lua
+++ cfg_statusbar.lua
@@ -36,7 +36,7 @@ mod_statusbar.create{
     -- right, respectively, and %systray is a placeholder for system tray
     -- windows and icons.
     --
-    template="%systray%filler%load | %date",
+    template="%systray%filler%linuxbatt%linuxbatt_state | %load | %date",
     --template="[ %date || load:% %>load || mail:% %>mail_new/%>mail_total ] %filler%systray",
     --template="[ %date || load: %05load_1min || mail: %02mail_new/%02mail_total ] %filler%systray",
 }
