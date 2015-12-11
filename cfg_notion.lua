1ebd9cda8abc08848523045813444f9bb2bbfb3c
Moritz Wilhelmy <mw@barfooze.de>
2015-12-11 09:47:57 +0100

    merge cfg_defaults.lua into cfg_notion.lua

--- cfg_notion.lua
+++ cfg_notion.lua
@@ -20,7 +20,7 @@ META="Mod4+"
 ALTMETA="Mod4+Shift+"
 
 -- Terminal emulator
-XTERM="urxvtc"
+XTERM="urxvtcd"
 
 -- Some basic settings
 ioncore.set{
@@ -62,32 +62,27 @@ ioncore.set{
     --mousefocus="sloppy",
 }
 
-
--- Load default settings. The file cfg_defaults loads all the files
--- commented out below, except mod_dock. If you do not want to load
--- something, comment out this line, and uncomment the lines corresponding
--- the the modules or configuration files that you want, below.
--- The modules' configuration files correspond to the names of the 
--- modules with 'mod' replaced by 'cfg'.
-dopath("cfg_defaults")
+--
+-- Notion default settings
+--
 
 -- Load configuration of the Notion 'core'. Most bindings are here.
---dopath("cfg_notioncore")
+dopath("cfg_notioncore")
 
 -- Load some kludges to make apps behave better.
---dopath("cfg_kludges")
+dopath("cfg_kludges")
 
 -- Define some layouts. 
---dopath("cfg_layouts")
+dopath("cfg_layouts")
 
 -- Load some modules. Bindings and other configuration specific to modules
 -- are in the files cfg_modulename.lua.
---dopath("mod_query")
---dopath("mod_menu")
---dopath("mod_tiling")
---dopath("mod_statusbar")
+dopath("mod_query")
+dopath("mod_menu")
+dopath("mod_tiling")
+dopath("mod_statusbar")
 --dopath("mod_dock")
---dopath("mod_sp")
+dopath("mod_sp")
 --dopath("mod_notionflux")
 dopath("mod_xrandr")
 
