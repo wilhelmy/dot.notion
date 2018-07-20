1258f8c6588cd4872c34eb26c69382549a48eda4
Moritz Wilhelmy <mw@barfooze.de>
2018-07-20 16:14:24 +0200

    inputplug.sh: explicitly use bash (source cmd w/ args), add debugging code

--- inputplug.sh
+++ inputplug.sh
@@ -1,8 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 # Hook script for https://bitbucket.org/andrew_shadura/inputplug/src/default/
 # Called whenever a mouse or keyboard is attached, I use it to set the keyboard
 # layout because X loses it.
 # Command: inputplug -c ~/.notion/inputplug.sh
+
+#exec >>/tmp/inputplug.log 2>&1 # enable for debugging
+
+echo $(date): $@
+set -ex
 
 action=$1
 dev=$3
@@ -16,3 +21,5 @@ case "$action:$dev" in
         source ~/.notion/keyboard.sh
     ;;
 esac
+
+echo
