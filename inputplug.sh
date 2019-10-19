59864de1a6ca2f2c30e790bc053b1b54ed58a37b
Moritz Wilhelmy <mw@barfooze.de>
2019-10-19 21:36:58 +0200

    explain what this is

--- inputplug.sh
+++ inputplug.sh
@@ -1,10 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Hook script for https://bitbucket.org/andrew_shadura/inputplug/src/default/
 # Called whenever a mouse or keyboard is attached, I use it to set the keyboard
-# layout because X loses it.
+# layout because X loses it, and to detect when I dock/undock my laptop (to
+# automatically reenable the xrandr setup) because HP laptops apparently don't
+# generate listenable events in that case.
+#
 # Command: inputplug -c ~/.notion/inputplug.sh
 
-#exec >>/tmp/inputplug.log 2>&1 # enable for debugging
+#exec >>/tmp/inputplug.log 2>&1 # uncomment for debug log
 
 echo $(date): $@
 set -ex
